0ed2a31b7ca62648527b13bb62e063fb648a0dd3
Sam Harwell <sam.harwell@rackspace.com>
2014-10-16 08:36:31 -0500

    Add stub IWizard implementation

---
+++ Sample.TemplateWizards/MyWizard.cs
@@ -0,0 +1,39 @@
+﻿namespace Sample.TemplateWizards
+{
+    using System;
+    using System.Collections.Generic;
+    using EnvDTE;
+    using Microsoft.VisualStudio.TemplateWizard;
+
+    public class MyWizard : IWizard
+    {
+        #region IWizard Members
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
